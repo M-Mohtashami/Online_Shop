94dfb09379b6f2620d77254ab6c9186196d60b0d
m-mohtashami <m.mohtashami88@gmail.com>
2023-06-03 23:54:14 +0330

    add iran sans font and config tailwind css library to use custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'iran-sans': ['IranSans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };

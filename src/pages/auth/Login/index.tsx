982eb0a264c87e75063b5e3d9f4057ee53a1a12f
m-mohtashami <m.mohtashami88@gmail.com>
2023-06-04 11:20:21 +0330

    recover login page changes

--- src/pages/auth/Login/index.tsx
+++ src/pages/auth/Login/index.tsx
@@ -1,6 +1,8 @@
 import Button from '@/components/shared_components/Button';
+import TextField from '@/components/shared_components/TextField';
 import { NextPageWithLayout } from '@/interfaces/inretfaces';
 import MainLayout from '@/layout/MainLayout';
+import Link from 'next/link';
 import React, { ReactElement } from 'react';
 
 const Login: NextPageWithLayout = () => {
@@ -27,46 +29,18 @@ const Login: NextPageWithLayout = () => {
           </p>
         </div> */}
 
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  {'نام کاربری'}
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    autoComplete="username"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  {'رمز عبور'}
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
+        <div className=" sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="relative bg-white border border-gray-200 py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <div className="absolute -top-16 right-0 w-full flex justify-center">
+              <img
+                className="w-24"
+                src="/assets/images/site_logo.png"
+                alt="logo"
+              />
+            </div>
+            <form className="space-y-6">
+              <TextField label="نام کابری" name="username" type="text" />
+              <TextField label="رمز عبور" name="password" type="password" />
 
               <div className="flex items-center justify-between">
                 <div className="flex items-center gap-3">
@@ -89,7 +63,8 @@ const Login: NextPageWithLayout = () => {
                 <Button
                   icon="forward"
                   type="submit"
-                  className="w-full flex flex-row-reverse gap-3 items-end justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  variant="contained"
+                  className="w-full "
                 >
                   {'ورود'}
                 </Button>
@@ -100,13 +75,14 @@ const Login: NextPageWithLayout = () => {
             <Button
               type="button"
               icon="backward"
-              className="w-16 p-2 border-2 border-red-500 text-red-500  rounded-full flex items-end gap-1 mt-3 "
+              variant="outlined"
+              className="w-20 mt-3 "
             >
               {'خانه'}
             </Button>
             <div>
               <span>{'کاربر تازه هستم: '}</span>
-              <a href="">{'ثبت نام'}</a>
+              <Link href="/auth/register">{'ثبت نام'}</Link>
             </div>
           </div>
         </div>

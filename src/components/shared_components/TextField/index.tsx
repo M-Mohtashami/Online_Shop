4129548ec3ea20b776695a0054546fef61b9355e
m-mohtashami <m.mohtashami88@gmail.com>
2023-06-05 02:10:26 +0330

    add validation to login form with react hook form and zod and controlled TextField component

--- src/components/shared_components/TextField/index.tsx
+++ src/components/shared_components/TextField/index.tsx
@@ -2,30 +2,28 @@ import React, { ReactElement } from 'react';
 
 type Props = {
   label: string;
-  name: string;
   type: string;
   error?: string | undefined;
   //   className: string;
+  controller: any;
   [key: string]: any;
 };
 
 const TextField = ({
   label,
-  name,
   type,
   error,
+  controller,
   //   className,
   ...inputProps
 }: Props) => {
   return (
     <div>
-      <label htmlFor={name} className="block text-sm font-medium">
-        {label}
-      </label>
+      <label className="block text-sm font-medium">{label}</label>
       <div className="mt-1">
         <input
-          name={name}
           type={type}
+          {...controller}
           {...inputProps}
           className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         />

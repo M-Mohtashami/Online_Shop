24a11baf99f4fc28eb71890f2bbaa15638732e5a
m-mohtashami <m.mohtashami88@gmail.com>
2023-06-11 23:54:51 +0330

    change next config for next Image component

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+      },
+    ],
+  },
+};

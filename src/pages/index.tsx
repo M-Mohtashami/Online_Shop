3b032d9f5a81953c30824e1c5a3e9db7a2010a9d
m-mohtashami <m.mohtashami88@gmail.com>
2023-06-21 18:33:41 +0330

    get products based on newest , popularity , special and cheepest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import getAllCategoryService from '@/api/services/category/getAllCategoryService
 import getAllSubCategoryService from '@/api/services/category/getAllSubCategoryService';
 import getAllProductsSevices from '@/api/services/product/getAllProductsServices';
 import Card from '@/components/Products/Card';
-import { NextPageWithLayout, ProductType } from '@/interfaces/inretfaces';
+import {
+  CategoryType,
+  NextPageWithLayout,
+  ProductType,
+} from '@/interfaces/inretfaces';
 import MainLayout from '@/layout/MainLayout';
 import { GetServerSideProps } from 'next';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
@@ -15,15 +19,21 @@ import { MutableRefObject, useCallback, useRef } from 'react';
 import MainSlider from '@/components/Home/MainSlider';
 
 type Props = {
-  products: {
+  categories: {
     status: string;
     page: number;
     per_page: number;
     total: number;
     total_pages: number;
     data: {
-      products: ProductType[];
+      categories: CategoryType[];
     };
+  };
+  products: {
+    newests: ProductType[];
+    specials: ProductType[];
+    cheepests: ProductType[];
+    populars: ProductType[];
   };
 };
 
@@ -50,8 +60,8 @@ const swiperParams = {
   },
 };
 
-const Home: NextPageWithLayout = ({ products }: Props) => {
-  const { products: productsData } = products?.data;
+const Home: NextPageWithLayout = ({ products, categories }: Props) => {
+  const { newests, specials, cheepests, populars } = products;
   const sliderRef = useRef(null);
 
   const handlePrev = useCallback((sliderRef: MutableRefObject<any>) => {
@@ -67,7 +77,7 @@ const Home: NextPageWithLayout = ({ products }: Props) => {
   return (
     <>
       <div className="mb-10">
-        <MainSlider slides={productsData} />
+        <MainSlider slides={newests} />
       </div>
       <div className="space-y-3 mt-32">
         <div className="text-primary text-xl px-6 border-b border-gray-300 pb-3 shadow-sm w-full flex items-center justify-between">
@@ -91,7 +101,7 @@ const Home: NextPageWithLayout = ({ products }: Props) => {
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
           >
-            {productsData.map((product) => (
+            {newests.map((product) => (
               <SwiperSlide key={product._id}>
                 <Card product={product} />
               </SwiperSlide>
@@ -127,7 +137,7 @@ const Home: NextPageWithLayout = ({ products }: Props) => {
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
           >
-            {productsData.map((product) => (
+            {specials.map((product) => (
               <SwiperSlide key={product._id}>
                 <Card product={product} />
               </SwiperSlide>
@@ -163,7 +173,7 @@ const Home: NextPageWithLayout = ({ products }: Props) => {
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
           >
-            {productsData.map((product) => (
+            {populars.map((product) => (
               <SwiperSlide key={product._id}>
                 <Card product={product} />
               </SwiperSlide>
@@ -199,7 +209,7 @@ const Home: NextPageWithLayout = ({ products }: Props) => {
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
           >
-            {productsData.map((product) => (
+            {cheepests.map((product) => (
               <SwiperSlide key={product._id}>
                 <Card product={product} />
               </SwiperSlide>
@@ -227,11 +237,19 @@ Home.getLayout = function getLayout(page: React.ReactElement) {
 export const getServerSideProps: GetServerSideProps = async () => {
   const categories = await getAllCategoryService();
   const subCategories = await getAllSubCategoryService();
-  const products = await getAllProductsSevices('');
+  const newests = await getAllProductsSevices('?sort=-createdAt&limit=10');
+  const specials = await getAllProductsSevices('?sort=-quantity&limit=10');
+  const cheepests = await getAllProductsSevices('?sort=price&limit=10');
+  const populars = await getAllProductsSevices('?sort=-rating.rate&limit=10');
 
   return {
     props: {
-      products: products,
+      products: {
+        newests: newests.data.products,
+        spetials: specials.data.products,
+        cheepests: cheepests.data.products,
+        populars: populars.data.products,
+      },
       categories: categories,
       subcategories: subCategories,
     },

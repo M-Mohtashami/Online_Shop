e92c27e4f973dc9494b9761d4dfba32c7c6bd6e3
m-mohtashami <m.mohtashami88@gmail.com>
2023-06-25 19:29:57 +0330

    add sum generic type to custom hook

--- src/hooks/product/useGetAllProducts.ts
+++ src/hooks/product/useGetAllProducts.ts
@@ -1,10 +1,13 @@
 import getAllProductsSevices from '@/api/services/product/getAllProductsServices';
 import { UseQueryOptions, useQuery } from '@tanstack/react-query';
 
-export const useGetAllProducts = (options: UseQueryOptions, query: string) => {
+export const useGetAllProducts = (
+  options: UseQueryOptions<unknown, unknown, unknown, any>,
+  query: string
+) => {
   return useQuery({
     ...options,
-    // queryKey: ['products', query],
+    queryKey: ['products', query],
     queryFn: () => getAllProductsSevices(query),
   });
 };

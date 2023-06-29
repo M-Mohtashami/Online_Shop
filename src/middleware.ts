d68a36cb7462587c3f6a852d49b459bcbdf43981
m-mohtashami <m.mohtashami88@gmail.com>
2023-06-29 21:18:06 +0330

    add some redirection in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import { routes } from './config/routes';
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('access_token');
+  const user = request.cookies.get('user_role')?.value;
 
   // console.log(token);
   if (request.nextUrl.pathname === routes.protected.Logout) {
@@ -12,16 +13,33 @@ export function middleware(request: NextRequest) {
     }
   }
   if (request.nextUrl.pathname.startsWith('/auth')) {
-    if (token) return NextResponse.redirect(new URL('/admin', request.url));
+    if (token && user === 'ADMIN')
+      return NextResponse.redirect(new URL('/admin', request.url));
+    if (token && user !== 'ADMIN') {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+    if (request.nextUrl.pathname === '/auth')
+      return NextResponse.redirect(
+        new URL(routes.protected.Login, request.url)
+      );
   }
 
   if (request.nextUrl.pathname.startsWith('/admin')) {
     // This logic is only applied to /dashboard
-    if (!token) return NextResponse.redirect(new URL('/', request.url));
+    if (!token || user !== 'ADMIN') {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
+  if (request.nextUrl.pathname.startsWith('/checkout')) {
+    // This logic is only applied to /dashboard
+    if (!token)
+      return NextResponse.redirect(
+        new URL(routes.protected.Login, request.url)
+      );
   }
   NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/admin/:path*', '/auth/:path*'],
+  matcher: ['/admin/:path*', '/auth/:path*', '/checkout/:path*'],
 };

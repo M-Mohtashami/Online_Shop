94dd4a82fd6f3db6ef28f635f4e0d6ce588ce793
m-mohtashami <m.mohtashami88@gmail.com>
2023-06-29 15:21:20 +0330

    add middleware to redux store

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,6 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '@/redux/slice';
-import { PersistConfig, persistReducer } from 'redux-persist';
+import {
+  PersistConfig,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { CartStateType } from '@/interfaces/inretfaces';
 import { WebStorage } from 'redux-persist/lib/types';
@@ -18,6 +27,12 @@ const store = configureStore({
   reducer: {
     cart: persistedCartReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;

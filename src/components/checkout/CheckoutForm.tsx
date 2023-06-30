8b2ad2e55525788e130bb8432a35e95c6e999e5d
m-mohtashami <m.mohtashami88@gmail.com>
2023-06-30 11:24:23 +0330

    change checkout form validation

--- src/components/checkout/CheckoutForm.tsx
+++ src/components/checkout/CheckoutForm.tsx
@@ -204,9 +204,6 @@ const CheckoutForm = ({ user }: Props) => {
               {...register('deliveryDate')}
               className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             /> */}
-            <small className="text-red-500">
-              {errors?.deliveryDate?.message?.toString()}
-            </small>
           </div>
 
           <small className="text-red-500">

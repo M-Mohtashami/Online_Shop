3ca56c17e17dc22575e91a6ca491bbafd2eae447
m-mohtashami <m.mohtashami88@gmail.com>
2023-07-02 23:04:20 +0330

    remove unused imports

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,10 +1,7 @@
 import { loginServices } from '@/api/services/loginServices';
 import { LoginValues } from '@/interfaces/inretfaces';
 import { UseMutationOptions, useMutation } from '@tanstack/react-query';
-import { toast } from 'react-toastify';
-import Cookies from 'universal-cookie';
 
-const cookie = new Cookies();
 const useLogin = (options: UseMutationOptions<any, any, any>) => {
   return useMutation({
     ...options,
